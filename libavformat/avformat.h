c4ca6b2c969935a4649f374036fd53f8b2cacd96
Nathan Lucas <22380816+nathanlucas@users.noreply.github.com>
2026-05-23 18:00:12 -0600

    temp fixes

--- libavformat/avformat.h
+++ libavformat/avformat.h
@@ -1104,6 +1104,11 @@ typedef struct AVStreamGroupTREF {
     unsigned int metadata_index;
 } AVStreamGroupTREF;
 
+typedef struct AVStreamGroupLayeredVideo {
+    const AVClass *av_class;
+    unsigned int el_index;
+} AVStreamGroupLayeredVideo;
+
 enum AVStreamGroupParamsType {
     AV_STREAM_GROUP_PARAMS_NONE,
     AV_STREAM_GROUP_PARAMS_IAMF_AUDIO_ELEMENT,
@@ -1111,6 +1116,7 @@ enum AVStreamGroupParamsType {
     AV_STREAM_GROUP_PARAMS_TILE_GRID,
     AV_STREAM_GROUP_PARAMS_LCEVC,
     AV_STREAM_GROUP_PARAMS_TREF,
+    AV_STREAM_GROUP_PARAMS_LAYERED_VIDEO,
 };
 
 struct AVIAMFAudioElement;
@@ -1154,6 +1160,7 @@ typedef struct AVStreamGroup {
         struct AVStreamGroupTileGrid *tile_grid;
         struct AVStreamGroupLCEVC *lcevc;
         struct AVStreamGroupTREF *tref;
+        struct AVStreamGroupLayeredVideo *layered_video;
     } params;
 
     /**
